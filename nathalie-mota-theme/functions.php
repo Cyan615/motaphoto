3525062b84055048f1ccf91992bdf32e12f68a9d
N.Maurice <cirilaii.nm@gmail.com>
2024-03-12 16:08:09 +0100

    déclaration des scripts

--- nathalie-mota-theme/functions.php
+++ nathalie-mota-theme/functions.php
@@ -9,6 +9,22 @@ function mota_setup(){
     add_theme_support( 'post-thumbnails' );
 
 };
+function mota_register_scripts(){
+
+    // Déclarer le JS
+	wp_enqueue_script( 'mota-js', get_template_directory_uri() . 'assets/js/script.js', array(), 
+    '1.0', true );
+    // Déclarer le css compilé sass
+    wp_enqueue_style('theme_style', get_template_directory_uri() . '/css/style.css', array(), '1.0.0' //filemtime(get_template_directory_uri() . '/assets/css/style.css')
+);
+    // Déclarer le css à la racine
+    wp_enqueue_style('style', get_stylesheet_uri() , array(),'1.0.0');
+
+}
+
 
 
 add_action('after_setup_theme', 'mota_setup');
+
+add_action('after_setup_theme', 'mota_register_scripts');
+
